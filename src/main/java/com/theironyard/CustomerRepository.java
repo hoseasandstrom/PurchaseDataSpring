ddfcef7b4f8c50938184486c97148f943507ce4f
hoseasandstrom <Lakyn4ever>
2016-06-22 13:44:26 -0400

    Added repository iterables

--- src/main/java/com/theironyard/CustomerRepository.java
+++ src/main/java/com/theironyard/CustomerRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by hoseasandstrom on 6/22/16.
  */
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-    public Customer findByName(String username);
+    public Customer findByName(String name);
+    public Customer findByEmail(String email);
 }
