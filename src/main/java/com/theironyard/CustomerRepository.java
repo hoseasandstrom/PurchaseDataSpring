44ca87f4609449f85cf72ec0f9600119e89685f6
hoseasandstrom <Lakyn4ever>
2016-06-22 13:28:08 -0400

    Customer repository set up

--- src/main/java/com/theironyard/CustomerRepository.java
+++ src/main/java/com/theironyard/CustomerRepository.java
@@ -1,7 +1,10 @@
 package com.theironyard;
 
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by hoseasandstrom on 6/22/16.
  */
-public interface CustomerRepository {
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+    public Customer findByName(String username);
 }
